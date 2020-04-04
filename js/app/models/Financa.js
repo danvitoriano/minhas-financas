4fa8a38278b59cc833a6f811da45325de5c3d07b
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-04-04 12:30:28 -0300

    Criando models js

---
+++ js/app/models/Financa.js
@@ -0,0 +1,25 @@
+class Financa { 
+
+    constructor(item, data, quantidade, valor) { 
+        this.item = item
+        this.data = data
+        this.quantidade = quantidade
+        this.valor = valor
+    }
+
+    getItem() { 
+        return this.item
+    }
+
+    getData() { 
+        return this.data
+    }
+
+    getQuantidade() { 
+        return this.quantidade
+    }
+
+    getValor() { 
+        return this.valor
+    }
+}
---
+++ js/app/models/Notificacao.js
@@ -0,0 +1,9 @@
+class Notificacao { 
+    setTexto(texto) { 
+        this.texto = texto
+    }
+
+    getTexto() { 
+        return this.texto
+    }
+}
