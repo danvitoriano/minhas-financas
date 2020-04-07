9917afd6e3b607e3c46f506a806dc30d61a963bf
fwfcunha <filipewalter1@gmail.com>
2020-04-06 21:06:10 -0300

    add

--- js/app/controller/FinancaController.js
+++ js/app/controller/FinancaController.js
@@ -1,11 +1,13 @@
 class FinancaController {
     constructor(){
+        this.inputItem = document.querySelector("#item")
         this.listaFinancas = new ListaFinancas()
     }
 
-    adiciona(){
+    adiciona(evento){
+        evento.preventDefault()
         this.listaFinancas.adiciona(new Financa())
-        this.listaFinancas.adiciona(new Financa("cinema", "06/04/2020", 1, 200))
+        this.listaFinancas.adiciona(new Financa(this.inputItem.value, "06/04/2020", 1, 200))
         console.log(this.listaFinancas)
     }    
 
